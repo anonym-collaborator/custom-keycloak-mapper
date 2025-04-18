7484c5d67fd74ef7eee4749387a488d81085ac65
anonym-collaborator <83634858+anonym-collaborator@users.noreply.github.com>
2025-04-18 12:37:28 +0300

    Update CustomRealmRoleOnlyMapper.java

--- src/main/java/com/example/keycloak/CustomRealmRoleOnlyMapper.java
+++ src/main/java/com/example/keycloak/CustomRealmRoleOnlyMapper.java
@@ -1,16 +1,24 @@
-
 package com.example.keycloak;
 
 import org.keycloak.models.*;
 import org.keycloak.protocol.oidc.mappers.*;
 import org.keycloak.protocol.oidc.*;
-import org.keycloak.representations.AccessToken;
+import org.keycloak.provider.*;
+import org.keycloak.representations.IDToken;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class CustomRealmRoleOnlyMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper {
+public class CustomRealmRoleOnlyMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper {
 
     public static final String PROVIDER_ID = "auto-custom-realm-role-mapper";
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, CustomRealmRoleOnlyMapper.class);
+    }
 
     @Override
     public String getDisplayCategory() {
@@ -33,20 +41,36 @@ public class CustomRealmRoleOnlyMapper extends AbstractOIDCProtocolMapper implem
     }
 
     @Override
-    public void transformAccessToken(AccessToken token, ProtocolMapperModel mappingModel,
-                                     KeycloakSession session, UserSessionModel userSession, ClientSessionContext clientSessionCtx) {
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
 
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession session) {
         RealmModel realm = session.getContext().getRealm();
         UserModel user = userSession.getUser();
 
         Set<RoleModel> realmRoles = user.getRealmRoleMappings();
 
-        for (RoleModel role : realmRoles) {
-            String roleName = role.getName();
+        RoleModel defaultRole = realm.getRole("default-roles-" + realm.getName());
 
-            if (!roleName.startsWith("default-roles-")) {
-                token.getRealmAccess().addRole(roleName);
-            }
+        // Filter out default roles and any composites containing it
+        Set<String> customRoles = realmRoles.stream()
+            .filter(role -> !includesRole(role, defaultRole))
+            .map(RoleModel::getName)
+            .collect(Collectors.toSet());
+
+        if (customRoles.size() == 1) {
+            String roleName = customRoles.iterator().next();
+            OIDCAttributeMapperHelper.mapClaim(token, mappingModel, roleName);
         }
+    }
+
+    private boolean includesRole(RoleModel role, RoleModel roleToExclude) {
+        if (role.equals(roleToExclude)) return true;
+        if (role.isComposite()) {
+            return role.getComposites().stream().anyMatch(r -> includesRole(r, roleToExclude));
+        }
+        return false;
     }
 }
